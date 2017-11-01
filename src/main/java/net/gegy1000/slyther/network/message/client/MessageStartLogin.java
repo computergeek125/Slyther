728344744ff746dfb7a67fb080dff669becb845c
Ryan <computergeek125@gmail.com>
2017-11-01 12:48:14 -0500

    fixed import

--- src/main/java/net/gegy1000/slyther/network/message/client/MessageStartLogin.java
+++ src/main/java/net/gegy1000/slyther/network/message/client/MessageStartLogin.java
@@ -3,6 +3,7 @@ package net.gegy1000.slyther.network.message.client;
 import net.gegy1000.slyther.client.SlytherClient;
 import net.gegy1000.slyther.network.MessageByteBuffer;
 import net.gegy1000.slyther.network.message.SlytherClientMessageBase;
+import net.gegy1000.slyther.network.message.server.MessageGotServerVersion;
 import net.gegy1000.slyther.server.ConnectedClient;
 import net.gegy1000.slyther.server.SlytherServer;
 import net.gegy1000.slyther.util.Log;
